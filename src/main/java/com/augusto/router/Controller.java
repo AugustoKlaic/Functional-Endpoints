01ec5c316703d16a319df7977f3ec296272a5fbc
AugustoKlaic <gutosopelsa@hotmail.com>
2020-11-05 23:37:58 -0300

    Added first test of functional endpoint

---
+++ src/main/java/com/augusto/router/Controller.java
@@ -0,0 +1,22 @@
+package com.augusto.router;
+
+import com.augusto.handler.HelloHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class Controller {
+
+    @Bean
+    public RouterFunction<ServerResponse> hello(HelloHandler helloHandler) {
+        return route()
+                .GET("/hello", accept(TEXT_PLAIN), helloHandler::hello).build();
+    }
+}
