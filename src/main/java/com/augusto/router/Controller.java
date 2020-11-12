3848fb29531a5d4f0e10fb68dabf8cb5c0fb1839
AugustoKlaic <gutosopelsa@hotmail.com>
2020-11-11 23:59:04 -0300

    Done first nested routes

--- src/main/java/com/augusto/router/Controller.java
+++ src/main/java/com/augusto/router/Controller.java
@@ -13,11 +13,22 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class Controller {
 
+//    @Bean
+//    public RouterFunction<ServerResponse> hello(HelloHandler helloHandler) {
+//        return route()
+//                .GET("/hello", accept(APPLICATION_JSON), helloHandler::hello)
+//                .GET("/hello/{name}", accept(APPLICATION_JSON), helloHandler::helloWithName)
+//                .build();
+//    }
+
     @Bean
-    public RouterFunction<ServerResponse> hello(HelloHandler helloHandler) {
-        return route()
-                .GET("/hello", accept(APPLICATION_JSON), helloHandler::hello)
-                .GET("/hello/{name}", accept(APPLICATION_JSON), helloHandler::helloWithName)
+    public RouterFunction<ServerResponse> nestedHello(HelloHandler helloHandler) {
+        return route().path("/hello", builder -> builder
+                .nest(accept(APPLICATION_JSON), routerBuilder -> routerBuilder
+                        .GET("", helloHandler::hello)
+                        .GET("/{name}", helloHandler::helloWithName)
+                ))
                 .build();
+
     }
 }
